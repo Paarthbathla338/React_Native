b75d7a7034206f9037319437f8b7815fb335df06
Paarthbathla338 <Paarth.bathla@outlook.com>
2021-10-01 09:44:10 +0530

    basic setup completed

---
+++ dc_clone/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Lets Build this Discord Clone 😎</h1>
+   
+    </div>
+  );
+}
+
+export default App;
