b33bb97cc0fad509ea4da6b170f4727685016de4
Paarthbathla338 <Paarth.bathla@outlook.com>
2021-10-05 20:48:12 +0530

    Basic Components Completed

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput,Button } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+    <View style={{padding:30,flexDirection: 'row',alignItems: 'center',justifyContent:"space-evenly"}}>
+    <TextInput
+      style={{
+        height: 50,
+        width:200,
+        borderBottomColor:"black",
+        borderBottomWidth:2,
+        marginLeft:5,
+        marginBottom:20
+        
+      }}
+      placeholder="Enter Your name"
+    />
+    <Button title="Say Hi!" 
+    style={{
+      width:"10",
+      height:"10"
+    }}/>
+
+    </View>
+    <View>
+
+
+    </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
